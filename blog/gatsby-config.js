f1d36003d344dadb5f67daa70dbf74551a7e9c19
Lint404 <klattkl@gmatc.matc.edu>
2022-08-28 19:40:01 -0500

    Added contact info.

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Kathryn Klatt`,
+    contact: {
+      Name: `Kathryn Klatt`,
+      Address: `9713 S 13th St`,
+      Address2: `Oak Creek, WI 53134`
+    },
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
