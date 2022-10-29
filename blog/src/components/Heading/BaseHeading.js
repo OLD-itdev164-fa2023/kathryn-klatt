e075c5ee2267e1e85b3d26572d35273fa010d1ea
Lint404 <klattkl@gmatc.matc.edu>
2022-10-29 16:57:50 -0500

    Completed Activity 7

--- blog/src/components/Heading/BaseHeading.js
+++ blog/src/components/Heading/BaseHeading.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Heading} from 'rebass'
+import { Heading } from 'rebass'
 
 const BaseHeading = ({ children, ...rest})=>
 <Heading {...rest}>
 {children}
 </Heading>
 
-BaseHeading.PropTypes = {
+BaseHeading.propTypes = {
     children: PropTypes.node.isRequired
 }
 
-export {BaseHeading}
+export { BaseHeading }
