e1b9916884815a3f338736a7ae54a64cbcba4f72
Lint404 <klattkl@gmatc.matc.edu>
2022-10-29 16:17:14 -0500

    Created and implemented H1 component.

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from 'styled-components'
+import {H1} from '../Heading'
 
 
 const Outer = styled.header`
@@ -12,10 +13,6 @@ padding: var(--space-4) var(--size-gutter);
 display: flex;
 align-items: center;
 justify-content: space-between;
-`
-
-const H1 = styled.h1`
-margin: 0px;
 `
 
 const StyledLink = styled(Link)`
