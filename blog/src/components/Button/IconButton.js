2d3771f69633a94b97bbf14b7ac366796e6b164c
Lint404 <klattkl@gmatc.matc.edu>
2022-11-05 15:04:22 -0500

    Created Icon Button Component.

---
+++ blog/src/components/Button/IconButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {BaseButton} from './BaseButton'
+
+const StyledButton = styled (BaseButton)`
+//inject theme styles here
+`
+
+export const IconButton = styled (({icon,...rest})=>{
+    let clone = React.cloneElement(icon, rest)
+    return <StyledButton as={clone.type} {...rest} className={rest.className}/>
+})
+`// Could add styles here but not necessary. This wraps styled icon which is passed in as a prop.`
+
+IconButton.defaultProps = {
+    size: 24
+}
+
+IconButton.propTypes = {
+    icon: PropTypes.node.isRequired
+}
