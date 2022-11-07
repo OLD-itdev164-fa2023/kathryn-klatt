ec7b687d806f4a2b1c7b950af22524300fc0386b
Lint404 <klattkl@gmatc.matc.edu>
2022-11-07 16:04:23 -0600

    Update Header Component

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -4,8 +4,9 @@ import { Link } from "gatsby"
 import styled from 'styled-components'
 import {H1} from '../Heading'
 import {Section} from '../Section'
-import {Search} from 'styled-icons/feather'
-import {IconButton} from '../Button'
+import {Flex} from 'rebass'
+import { SearchButton } from "../Button"
+import { ThemeConsumer } from "styled-components"
 
 
 const Outer = styled.header`
@@ -21,19 +22,55 @@ justify-content: space-between;
 const StyledLink = styled(Link)`
 color:white;
 text-decoration: none;
+margin: 0 10px;
 &:hover{color: lightgrey;}`
 
+const Image = styled.img`
+margin: 0;
+`
+
+const Nav = styled(Flex)`
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+`
+
+const Title = styled(H1)`
+flex: 4;
+`
+
+const MediaQuery = styled.div`
+@media (max-width: 450px){
+  display: none;
+}
+`
+
 const Header = ({ siteTitle }) => (
-<Outer>
-  <Section width={11/12}>
-    <H1>
-    <StyledLink to="/">{siteTitle}</StyledLink>
-    </H1>
-  </Section>
-<Section width={1/12}>
-<IconButton icon={<Search/>} variant='contrast'/>
-</Section>
-</Outer>
+  <Outer>
+    <Inner>
+      <Section flex>
+        <Section width={1 / 12} flex flexDirection="column" justifyContent="center">
+          <ThemeConsumer>
+            {theme => <Image src={theme.images.mainHeaderImage} />}
+          </ThemeConsumer>
+        </Section>
+        <Section
+          width={11 / 12} flex flexDirection="column" justifyContent="center">
+          <Nav>
+            <Title>
+            <StyledLink to="/">{siteTitle}</StyledLink>
+            </Title>
+            <MediaQuery>
+              <StyledLink to="/">Home</StyledLink>
+              <StyledLink to="/about">About</StyledLink>
+              <StyledLink to="/contact">Contact</StyledLink>
+            </MediaQuery>
+            <SearchButton icon={<Search />} variant="contrast" />
+          </Nav>
+        </Section>
+      </Section>
+    </Inner>
+  </Outer>
 )
 
 Header.propTypes = {
